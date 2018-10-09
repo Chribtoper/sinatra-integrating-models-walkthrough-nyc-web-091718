ce3380625e5b494198681ea73c9df0efbf67fecc
Trinacria <xcstolfax@gmail.comgit config --global user.name Christopher Stolfagit config --global user.email xcstolfax@gmail.com>
2018-10-09 17:41:59 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,5 +1,5 @@
 require_relative 'config/environment'
-require_relative 'models/text_analyzer.rb'
+require_relative "models/text_analyzer.rb"
 
 class App < Sinatra::Base
   get '/' do
@@ -9,6 +9,13 @@ class App < Sinatra::Base
   post '/' do
     text_from_user = params[:user_text]
 
+    @analyzed_text = TextAnalyzer.new(text_from_user)
+
     erb :results
   end
+
+  get '/' do
+    erb :results
+  end
+
 end
